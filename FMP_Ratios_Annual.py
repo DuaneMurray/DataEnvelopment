9ac1672b0999e36a25ec2100aa6b392f1e60405e
Duane Murray <duanemurrayaz@gmail.com>
2023-10-31 17:22:46 -0700

    Created import from REST API to MySQL RDBMS

--- FMP_Ratios_Annual.py
+++ FMP_Ratios_Annual.py
@@ -6,8 +6,23 @@
 # 
 # https://financialmodelingprep.com/api/v3/ratios/AAPL?period=quarter
 
+import os
+import mysql.connector
+import shutil
+from sqlalchemy import create_engine
+from glob import glob
 import requests
 import pandas as pd
+
+# USER, PW, AND DB ARE BEING IMPORTED FROM dbconfig.py FILE
+db_data = ("mysql+mysqlconnector://{user}:{pw}@localhost/{db}"
+           .format(user = "vsc",
+                   pw = "blaster123",
+                   db = "stockdata"))  
+
+# USING 'CREATE_ENGINE' FROM SQLALCHEMY TO MAKE THE DB CONNECTION
+engine1 = create_engine(db_data).connect()
+engine2 = create_engine(db_data).connect()
 
 # FMP API TOKEN MAY NEED TO USE '&' OR '?' AT BEGINING DEPENDING ON API ENDPOINT
 api_token = '&apikey=806a23dbb33fc1793a282fde9990c045' # ADD TO END OF REST API URL
@@ -15,33 +30,27 @@ api_token = '&apikey=806a23dbb33fc1793a282fde9990c045' # ADD TO END OF REST API 
 # FMP REST API URLs
 base_url = 'https://financialmodelingprep.com/api/v3/ratios/'  # ADD SYMBOL ON END OF URL
 
-debug_output = pd.DataFrame() # CREATE EMPTY DATAFRAME FOR OUTPUT TO SCREEN
-
 #########
 period = 'annual'              # OPTIONS: quarter, annual
-limit = '25'                   # MAX NUMBER OF PERIODS TO RETURN: MAX = 140
-data_directory = "Data/"        # DIRECTORY NAME UNDER THIS SCRIPTS LOCATION
-output_file_directory ="Stock-Fundamentals/Annual/"  # DIRECTORY NAME TO STORE OUTPUT CSV FILES
-stock_list = pd.read_csv(data_directory + "Symbols-TEST.csv") # LIST OF SYMBOLS
+limit = '35'                   # MAX NUMBER OF PERIODS TO RETURN: MAX = 140
 ##########
 
+##########
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM exchange_symbols WHERE symbol="AAPL" ORDER BY symbol', engine1)
+
+stock_list = pd.read_sql('SELECT DISTINCT symbol FROM exchange_symbols WHERE exchange="AMEX" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM exchange_symbols WHERE exchange="NYSE" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM exchange_symbols WHERE exchange="NASDAQ" ORDER BY symbol', engine1)
+##########
+
+
 # LOOP THROUGH THE LIST OF SYMBOLS INPUT FROM THE DEFINED stock_list FILE
-for x in stock_list['Symbol']:
+for x in stock_list['symbol']:
+
   # GET FINANCIAL RATIOS FOR FUNDAMENTAL ANALYSIS - QUICK RATIO, ETC
   resp = requests.get(base_url + x + '?period=' + period + '&limit=' + limit + api_token)
   resp.raise_for_status()
 
-  company_ratios = pd.DataFrame(resp.json()) # PUT THE JSON RESULTS INTO A DATAFRAME
-  company_ratios['period'] = period
+  df = pd.DataFrame(resp.json()) # PUT THE JSON RESULTS INTO A DATAFRAME
 
-  filename = x + '-Company-Ratios-Annual.csv'       # DEFINE THE FILENAME FOR CSV OUTPUT
-  
-  # GENERATE A CSV OUTPUT FILE IGNORING THE ROW INDEX NUMBERS
-  company_ratios.to_csv(data_directory + output_file_directory + filename, index=False)
-  
-  # CONCATENTATE DATAFRAMES FOR DISPLAY OUTPUT
-  debug_output = pd.concat([debug_output, company_ratios], ignore_index=True)
-  
-# CONVERT EPOCH DATE FORMAT
-#debug_output['date'] = pd.to_datetime(debug_output['date']/1000, unit="s")
-print(debug_output) # OUTPUT RESULTS TO SCREEN
+  df.to_sql(name='company_ratios', con=engine2, chunksize=5000, if_exists='append') # ADD DATA TO EXISTING TABLE

b3219be9fe808f1bcb435f9d029d9fa22fce5393
Duane Murray <duanemurrayaz@gmail.com>
2023-11-05 10:34:25 -0800

    Updated daily stock price import code

--- FMP_Daily_Prices.py
+++ FMP_Daily_Prices.py
@@ -6,8 +6,25 @@
 # 
 # https://financialmodelingprep.com/api/v3/historical-price-full/AAPL
 
+import sys
+
+import os
+import mysql.connector
+import shutil
+from sqlalchemy import create_engine
+from glob import glob
 import requests
 import pandas as pd
+
+# USER, PW, AND DB ARE BEING IMPORTED FROM dbconfig.py FILE
+db_data = ("mysql+mysqlconnector://{user}:{pw}@localhost/{db}"
+           .format(user = "vsc",
+                   pw = "blaster123",
+                   db = "stockdata"))  
+
+# USING 'CREATE_ENGINE' FROM SQLALCHEMY TO MAKE THE DB CONNECTION
+engine1 = create_engine(db_data).connect()
+engine2 = create_engine(db_data).connect()
 
 # FMP API TOKEN MAY NEED TO USE '&' OR '?' AT BEGINING DEPENDING ON API ENDPOINT
 api_token = '&apikey=806a23dbb33fc1793a282fde9990c045' # ADD TO END OF REST API URL
@@ -18,22 +35,66 @@ base_url = 'https://financialmodelingprep.com/api/v3/historical-price-full/'  # 
 #debug_output = pd.DataFrame() # CREATE EMPTY DATAFRAME FOR OUTPUT TO SCREEN
 
 #########
-start_date = '1998-01-01'
+start_date = '1991-01-01'
 end_date = ''
-data_directory = "Data/"
-output_file_directory ="Stock-Prices/"
-stock_list = pd.read_csv(data_directory + "Symbols-TEST.csv") # 1170 Symbols
-##########
 
-for x in stock_list['Symbol']:
+##########
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM exchange_symbols WHERE symbol="AAPL" ORDER BY symbol', engine1)
+
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM exchange_symbols WHERE exchange="AMEX" AND symbol NOT LIKE "%-%" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM exchange_symbols WHERE exchange="NYSE" AND symbol NOT LIKE "%-%" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM exchange_symbols WHERE exchange="NASDAQ" AND symbol NOT LIKE "%-%" ORDER BY symbol', engine1)
+
+#################################
+# SECTORS
+#################################
+#
+## Financial Services
+## Basic Materials
+## Consumer Defensive
+## Industrials
+## Energy
+## Healthcare
+## Communication Services
+## Consumer Cyclical
+## Technology
+## Real Estate
+## Utilities
+#
+# VIEW: company_data_set
+#
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Financial Services" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Basic Materials" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Consumer Defensive" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Industrials" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Energy" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Healthcare" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Communication Services" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Consumer Cyclical" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Technology" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Real Estate" ORDER BY symbol', engine1)
+#stock_list = pd.read_sql('SELECT DISTINCT symbol FROM company_data_set WHERE sector="Utilities" ORDER BY symbol', engine1)
+
+##########
+#sys.exit("Breakpoint -- sys.exit()")
+
+
+for x in stock_list['symbol']:
   
   # GET PRICE HISTORY FOR THE CURRENT STOCK IN THE LIST
   resp = requests.get(base_url + x + '?from='+ start_date +'&to='+ end_date + api_token)
   resp.raise_for_status()
   
-  data = resp.json()                    # GET THE JSON DATA FROM THE API
-  df = pd.DataFrame(data["historical"]) # PARSE THE HISTORICAL JSON SECTION
-  df['symbol'] = x                      # ADD THE STOCK SYMBOL TO THE DATAFRAME
+  df = resp.json()                    # GET THE JSON DATA FROM THE API
+
+  # MAKE SURE THE DATAFRAME HAS "historical" COLUMN
+  if "historical" in list(df):
+    df2 = pd.DataFrame(df["historical"]) # PARSE THE HISTORICAL JSON SECTION
+  
+    df2['symbol'] = x                      # ADD THE STOCK SYMBOL TO THE DATAFRAME
+    
+    df2.to_sql(name='equity_prices', con=engine2, chunksize=5000, if_exists='append') # ADD DATA TO EXISTING TABLE
+
 
   # VIEW NESTED JSON DATA RECEIVED FROM API - DEBUG OUTPUT
   #print("Symbol:", data["symbol"])
@@ -48,14 +109,3 @@ for x in stock_list['Symbol']:
   #  print(f"Change Percent: {entry['changePercent']}")
   #  print(f"Label: {entry['label']}")
   #  print()
-  
-  filename = x + '-Historical-Prices.csv'
-
-  # GENERATE A CSV OUTPUT FILE IGNORING THE ROW INDEX NUMBERS
-  df.to_csv(data_directory + output_file_directory + filename, index=False)
-
-  #debug_output = pd.concat([debug_output, df], ignore_index=True)
-
-# CONVERT EPOCH DATE FORMAT IF NEEDED
-#debug_output['date'] = pd.to_datetime(debug_output['date']/1000, unit="s")
-#print(debug_output) # OUTPUT RESULTS TO SCREEN

595791afed5dc3782e0a15eba528c15cf8e64c5d
Duane Murray <duanemurrayaz@gmail.com>
2023-11-08 13:38:05 -0800

    Market index SQL file

--- SQL/SQL-Market-Index.sql
+++ SQL/SQL-Market-Index.sql
@@ -1,4 +1,4 @@
-SELECT * FROM stockdata.market_index;
+#SELECT * FROM stockdata.market_index;
 
 # GET THE SP500 DATA
 #SELECT * FROM stockdata.market_index
@@ -11,3 +11,10 @@ SELECT * FROM stockdata.market_index;
 # GET THE NASDAQ INDEX DATA
 #SELECT * FROM stockdata.market_index
 #WHERE symbol = '^IXIC';
+
+SELECT symbol, date, close, 
+(((open - close)/ close)) AS 'pct_change'
+FROM market_index
+WHERE symbol= '^GSPC'
+#AND date = DATE_SUB('2015-12-31', INTERVAL 30 DAY) 
+AND (date BETWEEN DATE_SUB('2016-12-31', INTERVAL 1 MONTH) AND '2016-12-31')

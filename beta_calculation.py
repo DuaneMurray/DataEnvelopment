c9a4953eae48d7292e4d07b9e8c100f2a435977a
Duane Murray <duanemurrayaz@gmail.com>
2023-11-09 21:29:58 -0800

    Calculate 2015 Beta and Sigma Values

--- beta_calculation.py
+++ beta_calculation.py
@@ -11,6 +11,7 @@ from glob import glob
 import requests
 import pandas as pd
 from pandas_market_calendars import get_calendar
+import pandas_market_calendars as mcal
 import datetime
 from datetime import datetime
 import mysql.connector
@@ -38,6 +39,10 @@ engine2 = create_engine(db_data).connect()
 # CREATE A CURSORS TO THE DATABASE
 cursor = connection.cursor(buffered=True)
 mycursor = connection.cursor()
+
+######################################################################
+data = pd.DataFrame() # INIT EMPTY DATAFRAME OBJECT TO CONCAT FINAL OUTPUT
+df = pd.DataFrame()
 
 ##########################################
 # COMPARISON MARKET TO BENCHMARK AGAINST
@@ -68,11 +73,11 @@ sector_list = ["Financial Services", "Basic Materials", "Consumer Defensive", "I
 "Energy", "Healthcare", "Communication Services", "Consumer Cyclical", "Technology", "Real Estate",
 "Utilities"]
 
-#sector_list = ["Financial Services", "Utilities"] # TEST DATA
+#sector_list = ["Utilities"] # TEST DATA
 
 #######################################################
 # SET THE TIME FRAME FOR THE CALCULATIONS
-year = '2016'   # YEAR TO PROCESS
+year = '2015'   # YEAR TO PROCESS
 
 # LEAP YEARS: 
 # 2000, 2004, 2008, 2012, 2016, 2020, 2024
@@ -90,171 +95,203 @@ year = '2016'   # YEAR TO PROCESS
 # November: 30 days
 # December: 31 days
 
-date_start = year + '-01-01'   # JANUARY START DATE
-date_end = year + '-01-31'     # JANUARY END DATE
+month_start_dates = [year + '-01-01', year + '-02-01', year + '-03-01', year + '-04-01', year + '-05-01',
+                     year + '-06-01', year + '-07-01', year + '-08-01', year + '-09-01', year + '-10-01',
+                     year + '-11-01', year + '-12-01']
 
-#date_start = year + '-02-01'   # FEBRUARY START DATE
-#date_end = year + '-02-30'     # FEBRUARY END DATE
+month_end_dates = [year + '-01-31', year + '-02-28', year + '-03-31', year + '-04-30', year + '-05-31',
+                   year + '-06-30', year + '-07-31', year + '-08-31', year + '-09-30', year + '-10-31',
+                   year + '-11-30', year + '-12-31']
 
-#date_start = year + '-03-01'   # MARCH START DATE
-#date_end = year + '-03-31'     # MARCH END DATE
+counter = 0 # USED FOR DATE LIST INDEX ACCESS
 
-#date_start = year + '-04-01'   # APRIL START DATE
-#date_end = year + '-04-30'     # APRIL END DATE
+for date_start in month_start_dates:
+      
+      # GET THE PRE-DEFINED END OF MONTH VALUES
+      date_end = month_end_dates[counter]
+      counter += 1
 
-#date_start = year + '-05-01'   # MAY START DATE
-#date_end = year + '-05-31'     # MAY END DATE
+      # FIRST AND LAST TRADING DAYS FOR EACH HOLDING PERIOD
+      first_trading_days = []
+      last_trading_days = []
 
-#date_start = year + '-06-01'   # JUNE START DATE
-#date_end = year + '-06-30'     # JUNE END DATE
+      # GET THE VALID BUSINESS TRADING DAYS OF THE MONTH
+      us_calendar = get_calendar("BMF")
+      valid_range = us_calendar.valid_days(start_date=date_start, end_date=date_end)
 
-#date_start = year + '-07-01'   # JULY START DATE
-#date_end = year + '-07-31'     # JULY END DATE
+      first_trading_days.append(min(valid_range))
+      last_trading_days.append(max(valid_range))
 
-#date_start = year + '-08-01'   # AUGUST START DATE
-#date_end = year + '-08-31'     # AUGUST END DATE
+      # GET THE FIRST AND LAST TRADING DAYS OF THE MONTH
+      first_trading_day = first_trading_days[0].strftime("%Y-%m-%d")
+      last_trading_day = last_trading_days[0].strftime("%Y-%m-%d")
 
-#date_start = year + '-09-01'   # SEPTEMBER START DATE
-#date_end = year + '-09-30'     # SEPTEMBER END DATE
+      # GET THE CURRENT FINANCIAL QUARTER
+      q_date = pd.to_datetime(last_trading_day)
+      quarter = 'Q' + str(q_date.quarter)
 
-#date_start = year + '-10-01'   # OCTOBER START DATE
-#date_end = year + '-10-31'     # OCTOBER END DATE
+      # GET THE YEAR FOR EACH FINANCIAL QUARTER
+      year = last_trading_days[0].strftime("%Y")
 
-#date_start = year + '-11-01'   # NOVEMBER START DATE
-#date_end = year + '-11-30'     # NOVEMBER END DATE
-
-#date_start = year + '-12-01'   # DECEMBER START DATE
-#date_end = year + '-12-31'     # DECEMBER END DATE
-##########
-
-# FIRST AND LAST TRADING DAYS FOR EACH HOLDING PERIOD
-first_trading_days = []
-last_trading_days = []
-
-us_calendar = get_calendar("BMF")
-valid_range = us_calendar.valid_days(start_date=date_start, end_date=date_end)
-
-first_trading_days.append(min(valid_range))
-last_trading_days.append(max(valid_range))
-
-first_trading_day = first_trading_days[0].strftime("%Y-%m-%d")
-last_trading_day = last_trading_days[0].strftime("%Y-%m-%d")
-
-######################################################################
-data = pd.DataFrame() # INIT EMPTY DATAFRAME OBJECT TO CONCAT FINAL OUTPUT
-df = pd.DataFrame()
-
-######################################################################
-# BUILD S&P 500 INDEX DATAFRAME FOR BENCHMARK RATIO BASELINE
-# GET THE PERCENT CHANGE OF THE COMPARISON MARKET FOR BETA AND RETURN RATE CALCULATIONS
-
-# QUERY TO GET THE MARKET INDEX VALUE AT A GIVEN START DATE
-query_roi_mkt_start = ("SELECT date, symbol, close FROM market_index "
-             "WHERE symbol=%s AND date = %s")
-# QUERY TO GET THE MARKET INDEX VALUE AT A GIVEN END DATE   
-query_roi_mkt_end = ("SELECT date, symbol, close FROM market_index "
-             "WHERE symbol=%s AND date = %s")
-
-# EXECUTE THE QUERY; GRAB THE MARKET INDEX VALUE FOR THE START DATE
-#cursor.execute(query_roi_mkt_start, (market, date_start))
-cursor.execute(query_roi_mkt_start, (market, first_trading_day))
-sp500_start_result = cursor.fetchall()
-
-# EXECUTE THE QUERY; GRAB THE MARKET INDEX VALUE FOR THE END DATE
-#cursor.execute(query_roi_mkt_end, (market, date_end))
-cursor.execute(query_roi_mkt_end, (market, last_trading_day))
-sp500_end_result = cursor.fetchall()
-
-# CALCULATE THE MARKET INDEX RATE OF RETURN FOR THE DATE RANGE VALUES
-sp500_rate_of_return = (((sp500_end_result[0][2] - sp500_start_result[0][2]) / sp500_start_result[0][2]) * 100)
+      #sys.exit("Breakpoint -- sys.exit()")
 
 
-# PROCESS ALL SECTORS IN THE sector_list
-for market_symbol in sector_list:
+      # creating a date time object
+      #my_date = pd.Timestamp(2022, 4, 29)
 
-    symbol_query = ("SELECT symbol FROM company_details WHERE symbol NOT LIKE '%-%' AND sector=%s")
-    cursor.execute(symbol_query, (market_symbol,))
-    my_symbol_result = cursor.fetchall()
+      # obtaing the quarter of the given year
+      #print("The given date can be found in quarter ", my_date.quarter)
 
-    # LOOP THRU EACH SYMBOL RETURNED IN symbol_query SQL
-    for x in my_symbol_result:
+      ######################################################################
+      # BUILD S&P 500 INDEX DATAFRAME FOR BENCHMARK RATIO BASELINE
+      # GET THE PERCENT CHANGE OF THE COMPARISON MARKET FOR BETA AND RETURN RATE CALCULATIONS
 
-        # THE CURRENT SYMBOL TEXT - FOR CODE READABILITY
-        current_symbol = x[0]
+      # QUERY TO GET THE MARKET INDEX VALUE AT A GIVEN START DATE
+      #query_roi_mkt_start = ("SELECT date, symbol, close FROM market_index "
+      #        "WHERE symbol=%s AND date = %s")
+      query_roi_mkt_start = ("SELECT date, symbol, close FROM market_index "
+              "WHERE symbol=%s AND (date BETWEEN %s AND %s)")
+      # QUERY TO GET THE MARKET INDEX VALUE AT A GIVEN END DATE   
+      #query_roi_mkt_end = ("SELECT date, symbol, close FROM market_index "
+      #        "WHERE symbol=%s AND date = %s")
+      query_roi_mkt_end = ("SELECT date, symbol, close FROM market_index "
+              "WHERE symbol=%s AND (date BETWEEN %s AND %s)")
 
-        # QUERY TO GET THE STANDARD DEVIATION OVER THE DATE RANGE FOR THE STOCK PRICE
-        query_sigma = ("SELECT STDDEV(close) FROM equity_prices "
-                "WHERE symbol=%s AND (date BETWEEN %s AND %s)")
-            
-        # QUERY TO GET THE MARKET INDEX VALUE AT A GIVEN START DATE 
-        query_roi_start = ("SELECT date, symbol, close FROM equity_prices "
-                "WHERE symbol=%s AND date = %s")
-        
-        # QUERY TO GET THE MARKET INDEX VALUE AT A GIVEN END DATE 
-        query_roi_end = ("SELECT date, symbol, close FROM equity_prices "
-                "WHERE symbol=%s AND date = %s")
+      # EXECUTE THE QUERY; GRAB THE MARKET INDEX VALUE FOR THE START DATE
+      #cursor.execute(query_roi_mkt_start, (market, date_start))
+      cursor.execute(query_roi_mkt_start, (market, first_trading_day, last_trading_day))
+      sp500_start_result = cursor.fetchall()
 
-        #cursor.execute(query_sigma, (current_symbol, date_start, date_end))
-        cursor.execute(query_sigma, (current_symbol, first_trading_day, last_trading_day))
+      # EXECUTE THE QUERY; GRAB THE MARKET INDEX VALUE FOR THE END DATE
+      #cursor.execute(query_roi_mkt_end, (market, date_end))
+      cursor.execute(query_roi_mkt_end, (market, first_trading_day, last_trading_day))
+      sp500_end_result = cursor.fetchall()
 
-        # GET ALL THE RESULTS
-        myresult = cursor.fetchall()
+      # CALCULATE THE MARKET INDEX RATE OF RETURN FOR THE DATE RANGE VALUES
+      #sp500_rate_of_return = (((sp500_end_result[0][2] - sp500_start_result[0][2]) / sp500_start_result[0][2]) * 100)
+      sp500_rate_of_return = (((sp500_end_result[0][2] - sp500_end_result[-1][2]) / sp500_end_result[-1][2]))
 
-        # PLACE THE MYSQL OUTPUT RESULT LIST INTO A DATAFRAME OBJECT
-        df_sigma = pd.DataFrame(myresult)
+      # PROCESS ALL SECTORS IN THE sector_list
+      for market_symbol in sector_list:
+            symbol_query = ("SELECT symbol FROM company_details WHERE symbol NOT LIKE '%-%' AND sector=%s")
+            cursor.execute(symbol_query, (market_symbol,))
+            my_symbol_result = cursor.fetchall()
 
-        # GET THE BEGINNING OF THE TIMEFRAME CLOSING PRICE
-        #cursor.execute(query_roi_start, (current_symbol, date_start))
-        cursor.execute(query_roi_start, (current_symbol, first_trading_day))
-        myresult_roi_start = cursor.fetchall()
-        df_start = pd.DataFrame(myresult_roi_start)
+            # LOOP THRU EACH SYMBOL RETURNED IN symbol_query SQL
+            for x in my_symbol_result:
 
-        # GET THE ENDING OF THE TIMEFRAME CLOSING PRICE
-        #cursor.execute(query_roi_end, (current_symbol, date_end))
-        cursor.execute(query_roi_end, (current_symbol, last_trading_day))
-        myresult_roi_end = cursor.fetchall()
-        df_end = pd.DataFrame(myresult_roi_end)
+              # THE CURRENT SYMBOL TEXT - FOR CODE READABILITY
+              current_symbol = x[0]
+              #print(current_symbol)
 
-        # DROP ROWS WITH ANY EMPTY VALUES BEFORE PROCESSING
-        df_sigma.dropna(inplace=True)
-        df_start.dropna(inplace=True)
-        df_end.dropna(inplace=True)
+              # QUERY TO GET THE STANDARD DEVIATION OVER THE DATE RANGE FOR THE STOCK PRICE
+              query_sigma = ("SELECT STDDEV(close) FROM equity_prices "
+                      "WHERE symbol=%s AND (date BETWEEN %s AND %s)")
+                
+              # QUERY TO GET THE MARKET INDEX VALUE AT A GIVEN START DATE 
+              query_roi_start = ("SELECT date, symbol, close FROM equity_prices "
+                      "WHERE symbol=%s AND date = %s")
+                
+              # QUERY TO GET THE MARKET INDEX VALUE AT A GIVEN END DATE 
+              query_roi_end = ("SELECT date, symbol, close FROM equity_prices "
+                      "WHERE symbol=%s AND date = %s")
 
+              ############################################
+              # GET DATA FOR SLOPE REGRESSION FOR BETA
+              #
+              stock_query = ("SELECT close FROM equity_prices "
+                      "WHERE symbol=%s AND (date BETWEEN %s AND %s)")
 
-        if (not df_start.empty and  not df_end.empty):
-            rate_of_return = ((df_end[2] - df_start[2]) / df_start[2]) * 100
+              market_query = ("SELECT close FROM market_index "
+                      "WHERE symbol=%s AND (date BETWEEN %s AND %s)")
 
-            stock_beta = (rate_of_return / sp500_rate_of_return)
+              cursor.execute(stock_query, (current_symbol, date_start, date_end))
+              myresult = cursor.fetchall()
+              df_stock_query = pd.DataFrame(myresult)
 
-            stock_sigma = df_sigma[0][0]
+              cursor.execute(market_query, (market, date_start, date_end))
+              myresult = cursor.fetchall()
+              df_market_query = pd.DataFrame(myresult)
 
-            df[0] = first_trading_day
-            df[1] = last_trading_day
-            df[2] = current_symbol
-            df[3] = market_symbol
-            df[4] = rate_of_return
-            df[5] = sp500_rate_of_return
-            df[6] = stock_beta
-            df[7] = stock_sigma
-            df[8] = 'M'
+                # IF DATA EXISTS FOR THE STOCK AND THE SIZE OF PRICE ARRAY = SIZE OF MARKET ARRAY
+              if (not df_stock_query.empty and (df_stock_query.shape[0] == df_market_query.shape[0])):
+                      stock_price_change = df_stock_query.pct_change()
+                      market_price_change = df_market_query.pct_change()
 
-            data = pd.concat([data, df])
+                      stock_price_change.dropna(inplace=True)
+                      market_price_change.dropna(inplace=True)
 
-# REMOVE ANY ROWS THAT CONTAIN NaN
-data.dropna(inplace=True)
+                      # Create arrays for x and y variables in the regression model
+                      # Set up the model and define the type of regression
+                      x = np.array(stock_price_change).reshape((-1,1)) # STOCK SYMBOL PERCENT CHANGE
+                      y = np.array(market_price_change) # S&P 500 MARKET PERCENT CHANGE
+                      model = LinearRegression().fit(x, y) # SOLVE THE MODEL
+
+                      beta = model.coef_[0]
+
+                      #cursor.execute(query_sigma, (current_symbol, date_start, date_end))
+                      cursor.execute(query_sigma, (current_symbol, first_trading_day, last_trading_day))
+
+                      # GET ALL THE RESULTS
+                      myresult = cursor.fetchall()
+
+                      # PLACE THE MYSQL OUTPUT RESULT LIST INTO A DATAFRAME OBJECT
+                      df_sigma = pd.DataFrame(myresult)
+
+                      # GET THE BEGINNING OF THE TIMEFRAME CLOSING PRICE
+                      #cursor.execute(query_roi_start, (current_symbol, date_start))
+                      cursor.execute(query_roi_start, (current_symbol, first_trading_day))
+                      myresult_roi_start = cursor.fetchall()
+                      df_start = pd.DataFrame(myresult_roi_start)
+
+                      # GET THE ENDING OF THE TIMEFRAME CLOSING PRICE
+                      #cursor.execute(query_roi_end, (current_symbol, date_end))
+                      cursor.execute(query_roi_end, (current_symbol, last_trading_day))
+                      myresult_roi_end = cursor.fetchall()
+                      df_end = pd.DataFrame(myresult_roi_end)
+
+                      # DROP ROWS WITH ANY EMPTY VALUES BEFORE PROCESSING
+                      df_sigma.dropna(inplace=True)
+                      df_start.dropna(inplace=True)
+                      df_end.dropna(inplace=True)
+
+                      if (not df_start.empty and  not df_end.empty):
+                              rate_of_return = ((df_end[2] - df_start[2]) / df_start[2])
+
+                              stock_beta_1 = (rate_of_return / sp500_rate_of_return)
+
+                              stock_sigma = df_sigma[0][0]
+
+                              df[0] = first_trading_day
+                              df[1] = last_trading_day
+                              df[2] = quarter
+                              df[3] = year
+                              df[4] = current_symbol
+                              df[5] = market_symbol
+                              df[6] = rate_of_return
+                              df[7] = sp500_rate_of_return
+                              df[8] = beta
+                              df[9] = stock_sigma
+                              df[10] = 'M'
+                              df[11] = market
+
+                              data = pd.concat([data, df])
+
 
 # ADD COL NAMES AFTER DATAFRAME HAS BEEN BUILT
-data.columns = ['StartDate', 'EndDate', 'Symbol', 'Industry', 'StockReturnRate', 'MarketReturnRate', 'Beta', 'Sigma', 'Period']
-print(data)
+data.columns = ['StartDate', 'EndDate', 'Quarter', 'Year', 'Symbol', 'Industry', 'StockReturnRate', 'MarketReturnRate', 'Beta', 'Sigma', 'Period', 'MktIndex']
+#print(data)                              
+
+# REMOVE ANY ROWS THAT CONTAIN NaN VALUES FROM THE ANALYSIS
+data.dropna(inplace=True)
 
 #sys.exit("Breakpoint -- sys.exit()")
-
-#data.to_csv('pct_change_output.csv', index=False)
-
+                
+#data.to_csv('Beta-Output.csv', index=False)
 data.to_sql(name='company_beta_sigma', con=engine2, chunksize=5000, if_exists='append') # ADD DATA TO EXISTING TABLE
 
 ########################################################
-# CLOSE THE DATABASE CONNECTION AND REFERENCE CURSOR
+# CLOSE THE DATABASE CONNECTIONS AND REFERENCE CURSORS
 cursor.close()
 mycursor.close()
 connection.close()

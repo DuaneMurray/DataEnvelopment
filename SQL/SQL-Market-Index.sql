1699de725bf216f423c9765ef452894d263db001
Duane Murray <duanemurrayaz@gmail.com>
2023-11-10 13:56:18 -0800

    Added SQL statements

--- SQL/SQL-Market-Index.sql
+++ SQL/SQL-Market-Index.sql
@@ -1,8 +1,11 @@
 #SELECT * FROM stockdata.market_index;
 
 # GET THE SP500 DATA
-#SELECT * FROM stockdata.market_index
-#WHERE symbol = '^GSPC';
+SELECT * FROM stockdata.market_index
+WHERE symbol = '^GSPC'
+#WHERE symbol = '^DJI'
+#WHERE symbol = '^IXIC'
+AND (date BETWEEN '2019-12-01' AND '2019-12-31');
 
 # GET THE DOW JONES INDUSTRICAL DATA
 #SELECT * FROM stockdata.market_index
@@ -12,9 +15,11 @@
 #SELECT * FROM stockdata.market_index
 #WHERE symbol = '^IXIC';
 
-SELECT symbol, date, close, 
+/*
+SELECT symbol, date, open, close, 
 (((open - close)/ close)) AS 'pct_change'
 FROM market_index
 WHERE symbol= '^GSPC'
 #AND date = DATE_SUB('2015-12-31', INTERVAL 30 DAY) 
 AND (date BETWEEN DATE_SUB('2016-12-31', INTERVAL 1 MONTH) AND '2016-12-31')
+*/

b35bb0b744525340581db8e76b07cfded7d40bcf
Duane Murray <duanemurrayaz@gmail.com>
2023-11-15 12:56:47 -0800

    Updated DEA input & output SQL for new DB

--- SQL/SQL-Query-Get-DEA-Inputs-and-Outputs.sql
+++ SQL/SQL-Query-Get-DEA-Inputs-and-Outputs.sql
@@ -1,17 +1,51 @@
-SELECT DISTINCT p.symbol AS 'DMU',
-b.beta AS 'Beta', b.sigma AS 'Sigma', f.debtToEquity AS 'DToE', 
-f.pToBv AS 'PToB', f.netIncomeToRevenue AS 'NetMrgn', 
-f.incomeNetPerWabso AS 'EPS', f.returnOnAssets AS 'ROA', 
-f.returnOnEquity as 'ROE', b.rate_of_return as 'TTMreturn',
-b.investment_entry_price AS 'entryPrice', b.one_year_price AS 'oneYRprice', b.three_year_price AS 'threeYRprice',
-b.five_year_price AS 'fiveYRprice', Sector as 'Sector'
-FROM price_quotes p, financials f, beta_sigma b, securities s
-WHERE p.Symbol = b.Symbol AND p.Symbol = f.Symbol AND p.symbol = s.Symbol
-AND p.date > '2014-12-31' AND p.date < '2016-01-01'
-AND f.fiscalYear='2015'
-AND b.one_year_price IS NOT NULL
-AND b.three_year_price IS NOT NULL
-AND b.five_year_price IS NOT NULL
-AND f.returnOnEquity IS NOT NULL
-AND f.debtToEquity != 0
-AND s.Sector = 'Basic Materials';
+SELECT 
+	b.symbol AS 'DMU', 
+	#b.Year,
+    #b.Quarter,
+    #monthname(b.StartDate) AS 'Month',
+	b.beta AS 'Beta', 
+	b.sigma AS 'Sigma', 
+	r.debtEquityRatio AS 'DToE', 
+	r.priceToBookRatio AS 'PToB', 
+	r.netProfitMargin AS 'NetMrgn',
+    e.netIncomePerShare as 'EPS',
+	r.returnOnAssets AS 'ROA', 
+	r.returnOnEquity as 'ROE',
+	b.StockReturnRate as 'TrailingReturn',
+    p.Close AS 'entryPrice',
+    p.Close AS 'oneYRprice',
+    p.Close AS 'threeYRprice',
+    p.Close AS 'fiveYRprice',
+    b.Sector AS 'Sector'
+FROM 
+	stockdata.company_beta_sigma b, 
+	stockdata.company_ratios r,
+    stockdata.company_eps_pe e,
+    stockdata.equity_prices p
+WHERE 
+	b.Sector = 'Utilities'
+    AND monthname(b.StartDate) = 'January'
+    
+	AND b.Year = '2020'
+	AND b.Quarter = 'Q1'
+	AND r.calendarYear = '2020'
+	AND r.Period = 'Q1'
+    AND e.calendarYear = '2020'
+    AND e.period = 'Q1'
+    
+	AND b.Symbol = r.Symbol
+	AND p.Symbol = b.Symbol
+    AND e.Symbol = b.Symbol
+	
+    AND b.beta != 0
+	AND b.sigma != 0 
+	AND r.debtEquityRatio != 0
+	AND r.priceToBookRatio != 0
+	AND r.netProfitMargin != 0
+	AND e.netIncomePerShare != 0
+	AND r.returnOnAssets != 0 
+	AND r.returnOnEquity != 0
+	AND b.StockReturnRate != 0
+	AND p.Close != 0
+    
+    AND p.date = b.EndDate
